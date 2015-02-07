d4a2c2ec3287791ef45dea0723cca8a0931d06e1
Ben Liyanage <ben@perfectresolution.com>
2015-02-07 10:58:36 -0800

    url mappings

--- projects/DeveloperWeek2015/client/views.py
+++ projects/DeveloperWeek2015/client/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Candidate(request):
+    template = loader.get_template('client/questions.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
