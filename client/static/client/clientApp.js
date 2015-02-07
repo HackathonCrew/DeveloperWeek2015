f8a1b61759e15ca425644293f65a20e7a9e9bd11
mousterian <rivladi@gmail.com>
2015-02-07 15:12:24 -0800

    added click events to main page

--- client/static/client/clientApp.js
+++ client/static/client/clientApp.js
@@ -1,16 +1,53 @@
-var clientApp = angular.module('clientApp', [])
+var clientApp = angular.module('clientApp', []);
 
 
 //Do dynamic angular stuff
 clientApp.controller('ClientController', ['$scope', '$http', function($scope, $http){
+    $scope.clicked = false;
 
     $http.get('/api/get_statement')
         .success(function(data, status, headers, config){
             $scope.politicians = data;
-            // console.log(data);
         });
+
+    $http.get('/api/get_party_array')
+        .success(function(data, status, headers, config){
+            $scope.party = data.party;
+            // console.log(p)
+        });
+
+    $scope.reloadPage = function(){
+        window.location.reload();
+    }
+
+    $scope.selectParty = function(){
+        if (!$scope.clicked){
+            $scope.clicked = true;
+        }
+        else{
+            $scope.clicked = false;
+        }
+    }
 
 
 }]);
 
-//Do dynamic angular stuff
+clientApp.filter('capitalize', function() {
+    return function(input, scope) {
+        if (input!=null)
+        input = input.toLowerCase();
+        return input.substring(0,1).toUpperCase()+input.substring(1);
+    }
+});
+
+clientApp.directive('selectParty', function(){
+    var link = function(scope, element, attrs) {
+        element.bind('mousedown', function(){
+            element.addClass('liked');
+        })
+    }
+    return {
+        restrict:"AE",
+        link: link
+    }
+});
