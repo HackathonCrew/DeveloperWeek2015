61da177a7465067437ac07e2fd816cb2a47ca4f2
mousterian <rivladi@gmail.com>
2015-02-07 13:26:00 -0800

    images are loaded in the image src in client questions

--- projects/DeveloperWeek2015/client/static/client/clientApp.js
+++ projects/DeveloperWeek2015/client/static/client/clientApp.js
@@ -3,14 +3,11 @@ var clientApp = angular.module('clientApp', []);
 //Do dynamic angular stuff
 clientApp.controller('ClientController', ['$scope', '$http', function($scope, $http){
 
-    // $http.get('/api/get_statement')
-    //     .success(function(data, status, headers, config){
-    //         $scope.politicians = data.politicians;
-    //         $scope.$apply();
-    //         console.log(data);
+    $http.get('/api/get_statement')
+        .success(function(data, status, headers, config){
+            $scope.politicians = data;
+            // console.log(data);
+        });
 
-    //     });
-
-    $scope.politicians.image_url = "sdf";
 
 }]);
