fc57f2d475dc1dce467cbe485c0fe936d1c06fdf
mousterian <rivladi@gmail.com>
2015-02-07 13:04:40 -0800

    angluar

--- projects/DeveloperWeek2015/client/static/client/clientApp.js
+++ projects/DeveloperWeek2015/client/static/client/clientApp.js
@@ -1,3 +1,16 @@
-var clientApp = angular.module('clientApp', [])
+var clientApp = angular.module('clientApp', []);
 
 //Do dynamic angular stuff
+clientApp.controller('ClientController', ['$scope', '$http', function($scope, $http){
+
+    // $http.get('/api/get_statement')
+    //     .success(function(data, status, headers, config){
+    //         $scope.politicians = data.politicians;
+    //         $scope.$apply();
+    //         console.log(data);
+
+    //     });
+
+    $scope.politicians.image_url = "sdf";
+
+}]);
