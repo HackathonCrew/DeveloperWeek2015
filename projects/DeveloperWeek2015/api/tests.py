2c2fff02da811eeea62b6a556f229cfb898b62e1
emmaberyl <emma.beryl.roach@gmail.com>
2015-02-07 12:44:57 -0800

    updated api urls/views, added api unit test

--- projects/DeveloperWeek2015/api/tests.py
+++ projects/DeveloperWeek2015/api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+import views
+import json
 
 # Create your tests here.
+class TestBasic(TestCase):
+    "Basic tests"
+
+    def test_basic(self):
+        a = json.loads(views.getStatement())
+        self.assertEqual('Michele Bachman', a['politician'])
