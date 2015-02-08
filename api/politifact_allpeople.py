898af22d615d20b32362e18336a5f643c6e6f584
Ben Liyanage <ben@perfectresolution.com>
2015-02-07 16:17:07 -0800

    broke up unit tests and added people cache

---
+++ api/politifact_allpeople.py
@@ -0,0 +1,24 @@
+from django.core.cache import cache
+from requests import get
+from json import loads
+from random import choice
+
+def getAllPeople():
+    #get cache
+    politifact_people = cache.get('politifact_people')
+    
+    #test cache if None
+    if politifact_people == None:
+        #if None, fetch and cache All Users
+        r = get('http://www.politifact.com/api/people/all/json/')
+        politifact_people = loads(r.text)
+        
+        #TODO: filter out invalid users
+        
+        cache.set('politifact_people', politifact_people, None)
+        
+    #return cached users
+    return politifact_people
+    
+def randomPerson():
+    return choice(getAllPeople())
