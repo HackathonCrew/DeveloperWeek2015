789a2a5c47b37a05ddf6dc783bff9f300b3e0594
emmaberyl <emma.beryl.roach@gmail.com>
2015-02-07 18:24:45 -0800

    added face positioning to api call return

--- api/integrate_api.py
+++ api/integrate_api.py
@@ -1,10 +1,12 @@
 import json
 from sunlight_api import getImg, getID
 from politifact_allpeople import randomPerson
+from idol import getMouthCoordinates
 
 def getStatement():
 
     bio_id = ''
+    count = 0
 
     while not bio_id:
 
@@ -12,17 +14,18 @@ def getStatement():
         first_name = person['first_name']
         last_name = person['last_name']
         bio_id = getID(first_name,last_name)
+        count = count + 1
 
 
     image_url = getImg(bio_id)
     politician = '{0} {1}'.format(first_name,last_name)
 
     # statement,source_url,affiliation_index = function(name_slug)
-    # text_offset = function(image_url)
+    text_offset = getMouthCoordinates(image_url)
 
     statement = 'OH HAI'
     source_url = 'www.yourmom.com'
-    text_offset = [0,0]
+    # text_offset = [0,0]
     affiliation_index = 0
 
 
@@ -33,6 +36,7 @@ def getStatement():
         'image_url':image_url,
         'source_url':source_url,
         'text_offset':text_offset,
+        'hits':count
     }
 
     return json.dumps(result)
