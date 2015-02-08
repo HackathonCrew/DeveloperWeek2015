64f69485b3802962d4ff0899f8a4d5b015b7e4e7
emmaberyl <emma.beryl.roach@gmail.com>
2015-02-07 22:54:38 -0800

    added getRandomStatements (with an 's')

--- api/politifact_api.py
+++ api/politifact_api.py
@@ -17,3 +17,18 @@ def randomStatement(name_slug):
     'name_slug':str(data['speaker']['name_slug']), 'statement_type':str(data['statement_type']['statement_type']), 'party':str(data['speaker']['party']['party']), 'statement':str(data['statement'])}
 
     return politifact_dict
+
+def randomStatements(name_slug,n):
+    json_data = get('http://www.politifact.com/api/statements/truth-o-meter/people/{0}/json/?n={1}'.format(name_slug,n))
+    data = loads(json_data.text)
+    statements = []
+    statement_urls = []
+
+    for each in data:
+        statements.append(each['statement'])
+        statement_urls.append(each['statement_url'])
+
+    return statements
+
+
+
