272706d8a13437e2587ceec3fe14884b44c3a184
Ben Liyanage <ben@perfectresolution.com>
2015-02-08 14:10:08 -0800

    updates for find related

--- api/idol.py
+++ api/idol.py
@@ -18,8 +18,8 @@ def getMouthCoordinates(url):
         return ''
 
     return face
-    
-def addTextToIdol():
+
+def addAllPeopleToIdol():
 
     allPeople = politifact_allpeople.getAllPeople()
     count = 0
@@ -41,46 +41,108 @@ def addTextToIdol():
         elif person['party']['party']=='Journalist':
             continue
 
-        id = sunlight_api.getID(first_name,last_name)
-
-        if id:
-            result = politifact_api.randomStatements(name_slug)
-            if not result:
-                continue
-            image_url = sunlight_api.getImg(id)
-            text_param = getMouthCoordinates(image_url)
-
-            data = {
+        addTextToIdol(person)
+    
+def addTextToIdol(jsonDocument, index):
+    try:
+        print "------------------->adding text"
+        
+        r = requests.post("https://api.idolondemand.com/1/api/sync/addtotextindex/v1", 
+        data = 
+            {
+                'apikey': '31379774-e946-4bff-b26e-35e93ea7a67e',
+                'json': dumps({ 'document': jsonDocument }),
+                'index': index
+                
+                # 'text': '*'
+                # 'field_text': 'MATCH{Nancy Pelosi}:politician'
+            })
+        print r    
+        print r.text
+        print 'finished adding text'
+    except Exception as e:
+        print 'error'
+        print e
+        
+def findRelatedStatements(FullName):
+        r = requests.post("https://api.idolondemand.com/1/api/sync/findrelatedconcepts/v1",
+            data = 
+            {
+                'apikey':'31379774-e946-4bff-b26e-35e93ea7a67e',
+                'text': '*',
+                'field_text': 'MATCH{' + FullName + '}:politician',
+                'index': 'statements',
+            })
+        return r.text
             
-                'reference':id,
-                'title':'{0} {1}'.format(first_name,last_name),
-                #parametric field
-                'politician':'{0} {1}'.format(first_name,last_name),
-                #index field
-                'statement':result['statement'],
-                
-                #extra json
-                'party':result['party'],
-                'image_url':image_url,
-                'source_url':result['statement_url'],
-                'img_width':text_param['width'],
-                'img_top':text_param['top'],
-                'img_height':text_param['height'],
-                'img_offset':text_param['offset'],
-                'img_left':text_param['left'],
-                
-            }
-            json = dumps({ 'document': [ data ] })
-            try:
-                print "------------------->found match"
-                requests.get("https://api.idolondemand.com/1/api/sync/addtotextindex/v1", 
-                params = 
-                    {
-                        'apikey': '31379774-e946-4bff-b26e-35e93ea7a67e',
-                        'json': dumps({ 'document': [ data ] }),
-                        'index': 'politicians'
-                    })
-                count+=1
-            except (e):
-                print error
-                print e
+
+def addDocumentToIdol(statements, politicianName):
+    print "adding documents"
+    
+    documentArray = []
+    
+    for statement in statements:
+        myDocument = {
+            'reference':statement['url'],
+            'title': statement['statement'],
+            #parametric field
+            'politician':'{0}'.format(politicianName),
+            #index field
+            'statement':statement['statement'],
+            #extra fields
+            'sentiment': 1,
+        }
+        documentArray.append(myDocument)
+    
+    json = documentArray
+    addTextToIdol(json, 'statements')
+    
+    
+def addPersonToIdol(person):
+    print "adding person"
+    print person
+    print person['first_name'], person['last_name']
+    id = sunlight_api.getID(person['first_name'].strip(),person['last_name'].strip())
+    print id
+    if id:
+        print "getting results"
+        results = politifact_api.randomStatements(person['name_slug'],1000)
+        print "done getting results"
+        if not results:
+            print "no statements"
+            return
+            
+        print len(results)
+        print results
+        addDocumentToIdol(results, '{0} {1}'.format(person['first_name'],person['last_name']))
+        
+        statements = [myResult['statement'] for myResult in results]
+        
+        image_url = sunlight_api.getImg(id)
+        text_param = getMouthCoordinates(image_url)
+
+        politicianDocument = {
+        
+            'reference':id,
+            'title':'{0} {1}'.format(person['first_name'],person['last_name']),
+            #parametric field
+            'politician':'{0} {1}'.format(person['first_name'],person['last_name']),
+            #index field
+            'statement':statements,
+            
+            #extra json
+            'party':person['party'],
+            'image_url':image_url,
+            #'source_url':person['statement_url'],
+            'img_width':text_param['width'],
+            'img_top':text_param['top'],
+            'img_height':text_param['height'],
+            'img_offset':text_param['offset'],
+            'img_left':text_param['left'],
+            
+        }
+        json = [ politicianDocument ]
+        
+        addTextToIdol(json, 'politicians')
+
+# getRelatedConcepts('
