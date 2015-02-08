0dc2975e3e7e95be7c7b265982a8b87916ac2a40
mousterian <rivladi@gmail.com>
2015-02-07 22:43:11 -0800

    front-endy stuff

--- client/static/client/clientApp.js
+++ client/static/client/clientApp.js
@@ -1,14 +1,66 @@
-var clientApp = angular.module('clientApp', []);
+var clientApp = angular.module('clientApp', ['ngSanitize']);
 
 
 //Do dynamic angular stuff
-clientApp.controller('ClientController', ['$scope', '$http', function($scope, $http){
+clientApp.controller('ClientController', ['$scope', '$http', '$q', function($scope, $http, $q){
     $scope.clicked = false;
+    $scope.politician_array = [];
 
-    $http.get('/api/get_statement')
-        .success(function(data, status, headers, config){
-            $scope.politicians = data;
-        });
+    $scope.getData = function(){
+        console.log('requesting new politician');
+
+        promise = $http.get('/api/get_statement')
+            .success(function(data, status, headers, config){
+                // $scope.politicians = data;
+                console.log(data);
+                $('#test').lettering('words').children('span').lettering();
+                $scope.politician_array.push(data);
+                $scope.politicians = $scope.politician_array[0];
+                if ($scope.politician_array.length < 2)
+                {
+                    $scope.getData();
+                }
+            });
+
+        return promise;
+    }
+
+    $scope.showNewQuote = function(){
+        var myPromise;
+        if ($scope.politician_array.length <= 1){
+            myPromise = $scope.getData();
+            console.log('oldpoli', $scope.politicians);
+            myPromise.then(function(){
+                $scope.showNewQuote();
+            });
+
+            console.log('newpoli', $scope.politicians);
+            return;
+        }
+
+        $scope.politicians = $scope.politician_array[0];
+        $scope.politician_array.shift();
+        $scope.clicked = false;
+        $('#test').lettering();
+    }
+
+    $scope.showPolitician = function()
+    {
+        $scope.clicked = true;
+        var box = document.getElementById("box");
+
+        var facetop = $scope.politicians.text_offset.top;
+        var faceleft = $scope.politicians.text_offset.left + 150;
+        var faceheight = $scope.politicians.text_offset.height;
+        var facewidth = $scope.politicians.text_offset.width;
+
+        $(box).css({top: facetop, left: faceleft, height: faceheight, width: facewidth});
+
+        var bubble = document.getElementById("bubble");
+        var leftoffset = $scope.politicians.text_offset.offset[0] + 150;
+        var bottomoffset = $scope.politicians.text_offset.offset[1] + 120;
+        $(bubble).css({bottom: bottomoffset, left: leftoffset});
+    }
 
     $http.get('/api/get_party_array')
         .success(function(data, status, headers, config){
@@ -20,34 +72,27 @@ clientApp.controller('ClientController', ['$scope', '$http', function($scope, $h
         window.location.reload();
     }
 
-    $scope.selectParty = function(){
-        if (!$scope.clicked){
-            $scope.clicked = true;
-        }
-        else{
-            $scope.clicked = false;
-        }
-    }
-
-
 }]);
+
 
 clientApp.filter('capitalize', function() {
     return function(input, scope) {
-        if (input!=null)
-        input = input.toLowerCase();
-        return input.substring(0,1).toUpperCase()+input.substring(1);
+        if (input!=undefined)
+        {
+            input = input.toLowerCase();
+            return input.substring(0,1).toUpperCase()+input.substring(1);
+        }else {
+            return "";
+        }
     }
 });
 
-clientApp.directive('selectParty', function(){
-    var link = function(scope, element, attrs) {
-        element.bind('mousedown', function(){
-            element.addClass('liked');
-        })
-    }
-    return {
-        restrict:"AE",
-        link: link
-    }
+clientApp.directive("jqTable", function() {
+    console.log("kk");
+    // return function(scope, element, attrs) {
+    //     scope.$watch("assignments", function() {
+    //         $('#test').lettering();
+    //         console.log("fllflf");
+    //     });
+    // };
 });
